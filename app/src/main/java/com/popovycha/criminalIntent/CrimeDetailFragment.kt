e36bc1615005b0f07bc11f2cf1da1c2f3d174b59
popovycha <artem220397@gmail.com>
2023-12-22 14:58:32 -0500

    1st commit

---
+++ app/src/main/java/com/popovycha/criminalIntent/CrimeDetailFragment.kt
@@ -0,0 +1,63 @@
+package com.popovycha.criminalIntent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import com.popovycha.criminalIntent.databinding.FragmentCrimeDetailBinding
+import java.util.Date
+import java.util.UUID
+
+class CrimeDetailFragment : Fragment() {
+
+    private lateinit var crime: Crime
+
+    private var _binding: FragmentCrimeDetailBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+    }
+    //function where you inflate and bind the layout for the fragment’s
+    //view and return the inflated View to the hosting activity
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding =
+            FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+    //wire up the views
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+            crimeDate.apply {
+                text = crime.date.toString()
+                isEnabled = false
+            }
+            crimeSolved.setOnCheckedChangeListener { _, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/popovycha/criminalIntent/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.popovycha.criminalIntent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity(R.layout.fragment_crime_detail) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+
+
